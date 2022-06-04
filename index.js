9afab9bc3f28054cd2fd208e79f630cae48cfae1
Kevin Hoang <kevin.hoang322@gmail.com>
2022-06-04 11:51:51 -0500

    added more functions

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const {Intents} = require('discord.js');
 const dotenv = require('dotenv');
 const fetch = require('node-fetch');
 
+//setting up dotenv token
 dotenv.config();
 
+//intents from discord api
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -12,27 +14,68 @@ const client = new DiscordJS.Client({
     ]
 });
 
+//when bot ready
 client.on('ready', () =>{
     console.log('bot ready');
 });
 
 //curl https://api.animethemes.moe/video/?sort=random
-
 //animethemes api
-const urlVidRandom = "https://api.animethemes.moe/video/?sort=random";
+const urlAnimeThemes = "https://api.animethemes.moe/video/?sort=random";
 
-let fetchVidRandom = async (message) => {
-    fetch(urlVidRandom)
+//random op/ed vid function
+let fetchRandomVid = async (message) => {
+    fetch(urlAnimeThemes)
         .then( res => res.json() )
         .then( data => 
             message.reply({
-                content: data.videos[0].filename+'\n'+data.videos[0].link
+                content: data.videos[0].filename + '\n' + data.videos[0].link
             }));
 }
 
+//animechan api
+const urlAnimeChan = "https://animechan.vercel.app/api/random"
+
+//random quote function
+let fetchRandomQuote = async (message) => {
+    fetch(urlAnimeChan)
+        .then(res => res.json())
+        .then(data => 
+            message.reply({
+                content: 'Anime: ' + data.anime + '\n' + data.quote + '\n' + '-' + data.character
+            }));
+}
+
+//waifu pics api
+const urlWaifuPics = "https://api.waifu.pics/sfw/waifu"
+
+//random waifu pic function
+let fetchWaifuPic = async (message) => {
+    fetch(urlWaifuPics)
+        .then(res => res.json())
+        .then(data => 
+            message.reply({
+                content: data.url
+            }));
+}
+
+//Put other apis here and comment like above. If using same api, put it in the same section
+
+
+
+//Whenever user creates a message in discord channel
 client.on('messageCreate', async (message) =>{
-    if(message.content === 'ping'){
-        await fetchVidRandom(message);
+    switch(message.content){
+        case 'randomvid':
+            await fetchRandomVid(message);
+            break;
+        case 'randomquote':
+            await fetchRandomQuote(message);
+            break;
+        case 'randomwaifupic':
+            await fetchWaifuPic(message);
+            break;
+        default:
     }
 });
 
