e6029853d5a84e0bd3b7fe9218826cafe519059c
Kevin Hoang <kevin.hoang322@gmail.com>
2022-08-02 06:45:00 -0500

    set activity

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,16 @@ module.exports = {
     once: true,
     async execute (client, commands) {
         console.log('bot ready');
+        /*      
+            ActivityTypes
+            PLAYING
+            STREAMING
+            LISTENING
+            WATCHING
+            CUSTOM
+            COMPETING 
+        */
+        client.user.setActivity('?help or /help');
     }
 }
 
